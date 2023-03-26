b53e41c88e5c5d15a88a338b04c5dd8b591ff814
adi <adwibedi@buffalo.edu>
2023-03-26 16:23:33 -0400

    login features

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,6 @@
 import flask
 from pymongo import MongoClient
-
+import TicTacToe
 app = flask.Flask(__name__)
 
 
@@ -8,21 +8,26 @@ mongo_client = MongoClient('mongo')
 db = mongo_client['cse312Team']
 user_collection = db["users"]
 
-
+#HTML
 @app.route('/' ,methods=['GET', 'POST'])
 def loginhtml():
     return flask.send_file('../HTML/LoginPage.html')
-
-@app.route('/CSS/LoginPage.css' ,methods=['GET', 'POST'])
-def logincss():
-    return flask.send_file('../CSS/LoginPage.css')
-
 @app.route('/HTML/RegisterPage.html', methods=['GET', 'POST'])
 def registerhtml():
     return flask.send_file('../HTML/RegisterPage.html')
+
+
+#CSS
+@app.route('/CSS/LoginPage.css' ,methods=['GET', 'POST'])
+def logincss():
+    return flask.send_file('../CSS/LoginPage.css')
 @app.route('/CSS/RegisterPage.css' ,methods=['GET', 'POST'])
 def registercss():
     return flask.send_file('../CSS/RegisterPage.css')
+
+@app.route('/CSS/LandingPage.css' ,methods=['GET', 'POST'])
+def landingpagecss():
+    return flask.send_file('../CSS/LandingPage.css')
 
 
 @app.route('/MadeNewAccount' ,methods=['GET', 'POST'])
@@ -40,10 +45,10 @@ def made_new_account():
         store_stuff['username'] = username
         store_stuff['confirmed'] = confirmpassword
 
-        user_collection.insert_one(store_stuff)
+        #user_collection.insert_one(store_stuff)
 
 
-    return 'You have successfully made an account'
+    return flask.redirect('/tic-tac-toe')
 
 
 @app.route('/LoggedIn' ,methods=['GET', 'POST'])
@@ -53,18 +58,21 @@ def login():
         email = entire_data.get('email')
         password = entire_data.get('password')
 
-        print('Received email:', email)
-        print('Received password:', password)
-
         data = user_collection.find({})
 
         for item in data:
             if item['email'] == email and item['password'] == password:
-                return 'You have successfully logged in'
+                return flask.redirect('/tic-tac-toe')
             else:
                 return 'Invalid login/ password details'
 
 
+@app.route('/tic-tac-toe')
+def tictactoe():
+    game = TicTacToe.tic_tac_toe()
+    output = 'This is the output of my Tic Tac Toe Python code!'
+
+    return flask.send_file('../HTML/LandingPage.html')
 
 if __name__ == '__main__':
     app.run()
